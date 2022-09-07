fe20e371aa52d22316a305e79ace432d5ddf828c
Ahmet AKDEMIR <ahmet.akdemir@st.uskudar.edu.tr>
2022-09-07 09:26:41 +0300

    Generics final

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Ahmet");
+            Console.WriteLine(isimler.Length);
+            isimler.Add("mehmet");
+            Console.WriteLine(isimler.Length);
+            isimler.Add("recep");
+            Console.WriteLine(isimler.Length);
+
+            foreach (var items in isimler.Items)
+            {
+                Console.WriteLine(items);
+            }
+
+
+        }
+    }
+}
